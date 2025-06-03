f9fe53cbe18e3aa4e321c919b6b0e27dd9609136
Rodrigo Jonas Dittrich <rodrigo.jonas.dittrich@gmail.com>
2025-06-03 10:29:03 -0300

    Crie um projeto java com spring boot

---
+++ spring/src/main/java/com/example/docker_exemple/DockerExempleApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker_exemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerExempleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerExempleApplication.class, args);
+	}
+
+}
---
+++ spring/src/test/java/com/example/docker_exemple/DockerExempleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker_exemple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerExempleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
